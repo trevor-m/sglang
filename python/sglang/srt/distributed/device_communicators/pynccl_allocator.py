0dac62d288c1e50dcae63efd5237b06434af8bd0
Trevor Morris <tmorris@nvidia.com>
2025-07-16 00:40:13 +0000

    fix segfault

--- python/sglang/srt/distributed/device_communicators/pynccl_allocator.py
+++ python/sglang/srt/distributed/device_communicators/pynccl_allocator.py
@@ -6,26 +6,30 @@ from torch.cuda.memory import CUDAPluggableAllocator
 
 nccl_allocator_source = """
 #include <nccl.h>
+#include <c10/cuda/CUDAGuard.h>
 extern "C" {
 
 void* nccl_alloc_plug(size_t size, int device, void* stream) {
   void* ptr;
+  at::cuda::OptionalCUDAGuard gpuGuard(device);
   ncclResult_t err = ncclMemAlloc(&ptr, size);
   return ptr;
 
 }
 
 void nccl_free_plug(void* ptr, size_t size, int device, void* stream) {
+  at::cuda::OptionalCUDAGuard gpuGuard(device);
   ncclResult_t err = ncclMemFree(ptr);
 }
 
 }
 """
 
+_allocator = None
 _mem_pool = None
 
 def get_nccl_mem_pool():
-    global _mem_pool
+    global _allocator, _mem_pool
     if _mem_pool is None:
         out_dir = tempfile.gettempdir()
         nccl_allocator_libname = "nccl_allocator"
@@ -39,9 +43,9 @@ def get_nccl_mem_pool():
             build_directory=out_dir,
         )
 
-        allocator = CUDAPluggableAllocator(
+        _allocator = CUDAPluggableAllocator(
             f"{out_dir}/{nccl_allocator_libname}.so", "nccl_alloc_plug", "nccl_free_plug"
         ).allocator()
-        _mem_pool = torch.cuda.MemPool(allocator)
+        _mem_pool = torch.cuda.MemPool(_allocator)
     
     return _mem_pool
